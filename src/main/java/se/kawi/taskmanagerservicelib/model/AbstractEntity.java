0b35d2d5d1e8bbf0375610e8aab6a9c1b476368f
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-03-22 20:45:01 +0100

    Added itemKey to abstract entity

--- src/main/java/se/kawi/taskmanagerservicelib/model/AbstractEntity.java
+++ src/main/java/se/kawi/taskmanagerservicelib/model/AbstractEntity.java
@@ -1,8 +1,12 @@
 package se.kawi.taskmanagerservicelib.model;
 
 import javax.persistence.Id;
+
+import java.util.UUID;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
 
 @MappedSuperclass
 public abstract class AbstractEntity {
@@ -10,8 +14,21 @@ public abstract class AbstractEntity {
 	@Id
 	@GeneratedValue
 	private Long id;
+	
+	private String itemKey;
 
 	public Long getId() {
 		return id;
+	}
+	
+	public String getItemKey() {
+		return itemKey;
+	}
+	
+	@PrePersist
+	public void createKey() {
+		if (itemKey == null) {
+			itemKey = UUID.randomUUID().toString();
+		} 
 	}
 }
