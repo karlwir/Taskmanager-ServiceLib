bc40e8d9a7dc06c12d9801d7ae0d3861f55857b1
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-03-24 14:40:39 +0100

    More clean up in service layer

--- src/main/java/se/kawi/taskmanagerservicelib/service/TeamService.java
+++ src/main/java/se/kawi/taskmanagerservicelib/service/TeamService.java
@@ -13,10 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
-
 @Component
 public class TeamService extends BaseService<Team, TeamRepository> {
-	
+
 	private static final int TEAM_MAX_SIZE = 10;
 	private static final int USER_MAX_TEAMS = 3;
 
@@ -28,26 +27,30 @@ public class TeamService extends BaseService<Team, TeamRepository> {
 	public List<User> getTeamMembers(Specification<User> spec, Pageable pageable) throws ServiceException {
 		return userService.query(spec, pageable);
 	}
-	
+
 	public Team addTeamMember(String userItemKey, Team team) throws ServiceException {
-		if (team.getUsers().size() >= TEAM_MAX_SIZE) {
-			throw new ServiceException("Team is full");
-		}
-		
-		User user = userService.getByItemKey(userItemKey);
-		if (user.getTeams().size() >= USER_MAX_TEAMS) {
-			throw new ServiceException("User has to many teams: ");
-		}
-		team.addUser(user);
-		return super.save(team);
+		return execute(() -> {
+			if (team.getUsers().size() >= TEAM_MAX_SIZE) {
+				throw new ServiceDataException("Team is full. Max number of members in each team is: " + TEAM_MAX_SIZE);
+			}
+			User user = userService.getByItemKey(userItemKey);
+			if (user.getTeams().size() >= USER_MAX_TEAMS) {
+				throw new ServiceDataException(
+						"User has to many teams. Max number of teams per user is: " + USER_MAX_TEAMS);
+			}
+			team.addUser(user);
+			return super.save(team);
+		});
 	}
 
 	public Team removeTeamMember(String userItemKey, Team team) throws ServiceException {
-		User user = userService.getByItemKey(userItemKey);
-		team.removeUser(user);
-		return super.save(team);
+		return execute(() -> {
+			User user = userService.getByItemKey(userItemKey);
+			team.removeUser(user);
+			return super.save(team);
+		});
 	}
-	
+
 	public List<WorkItem> getTeamWorkItems(Specification<WorkItem> spec, Pageable pageable) throws ServiceException {
 		return workItemService.query(spec, pageable);
 	}
--- src/main/java/se/kawi/taskmanagerservicelib/service/WorkItemService.java
+++ src/main/java/se/kawi/taskmanagerservicelib/service/WorkItemService.java
@@ -2,12 +2,10 @@ package se.kawi.taskmanagerservicelib.service;
 
 import java.util.List;
 
-import javax.ws.rs.WebApplicationException;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import se.kawi.taskmanagerservicelib.model.Issue;
 import se.kawi.taskmanagerservicelib.model.WorkItem;
@@ -22,29 +20,31 @@ public class WorkItemService extends BaseService<WorkItem, WorkItemRepository> {
 		super(workItemRepository);
 	}
 
-	public List<Issue> getWorkItemIssues(Specification<se.kawi.taskmanagerservicelib.model.Issue> spec, Pageable pageable) throws ServiceException {
+	public List<Issue> getWorkItemIssues(Specification<se.kawi.taskmanagerservicelib.model.Issue> spec,
+			Pageable pageable) throws ServiceException {
 		return issueService.query(spec, pageable);
 	}
 
 	public WorkItem addIssueToWorkItem(String issueItemKey, WorkItem workItem) throws ServiceException {
-		if (!workItem.getStatus().equals(Status.DONE)) {
-			throw new ServiceException("Work item needs to be done", new WebApplicationException("Work item needs to be done", 400));
-		}
-		else {
-			return transaction(() -> {
+		return transaction(() -> {
+			if (!workItem.getStatus().equals(Status.DONE)) {
+				throw new ServiceDataException("Issues can only be added to work items with status DONE.");
+			} else {
 				Issue issue = issueService.getByItemKey(issueItemKey);
 				issue.setWorkItem(workItem);
 				workItem.setStatus(Status.UNSTARTED);
 				issueService.save(issue);
 				return save(workItem);
-			});
-		}		
+			}
+		});
 	}
-	
+
 	public WorkItem removeIssueFromWorkItem(String issueItemKey, WorkItem workItem) throws ServiceException {
-		Issue issue = issueService.getByItemKey(issueItemKey);
-		issue.setWorkItem(null);
-		issueService.save(issue);
-		return workItem;
+		return execute(() -> {
+			Issue issue = issueService.getByItemKey(issueItemKey);
+			issue.setWorkItem(null);
+			issueService.save(issue);
+			return workItem;
+		});
 	}
 }
